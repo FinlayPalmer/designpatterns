d9d0b22d175bfefdaeea0d4ca96ada6a3b68a3d2
Finlay Palmer <finlay.palmer@yahoo.com>
2024-09-08 21:55:39 -0400

    Finalized PotatoeHead

--- decorator/PotatoeHead.java
+++ decorator/PotatoeHead.java
@@ -10,7 +10,7 @@ public class PotatoeHead extends Character {
      * @param name The name of the PotatoeHead Character
      */
     public PotatoeHead(String name) {
-        super(FileReader.getLines("designpatterns/decorator/txt/potatoe-head.txt"), name);
+        super(FileReader.getLines("decorator/txt/potatoe-head.txt"), name);
     }
 
 }
