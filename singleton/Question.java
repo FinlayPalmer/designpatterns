6f60bab8d8c9eacda8d2ccedbf34070078bf3625
Finlay Palmer <finlay.palmer@yahoo.com>
2024-09-22 22:18:37 -0400

    added output.png and comments

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,10 +1,22 @@
 import java.util.ArrayList;
 
+/**
+ * Creates a new Question
+ * 
+ * @author Finlay Palmer
+ */
 public class Question {
     private String question;
     private ArrayList<String> answers;
     private int correctAnswer;
 
+    /**
+     * Creates a new Question
+     * 
+     * @param question      String representing the Question
+     * @param correctAnswer The correct number of the answer to the question
+     * @param answers       All possible answers to the question
+     */
     public Question(String question, int correctAnswer, String[] answers) {
         this.question = question;
         this.correctAnswer = correctAnswer;
@@ -14,15 +26,31 @@ public class Question {
         }
     }
 
+    /**
+     * Returns a String representing the question
+     * 
+     * @return A String representing the question
+     */
     public String toString() {
-        return question + "\n1. " + answers.get(0) + "\n2. " + answers.get(1) + "\n3. " + answers.get(2) + "\n4. "
-                + answers.get(3);
+        return "\n" + question + "\n 1. " + answers.get(0) + "\n 2. " + answers.get(1) + "\n 3. " + answers.get(2)
+                + "\n 4. " + answers.get(3) + "\n";
     }
 
+    /**
+     * Returns if the user's answer is correct
+     * 
+     * @param userAnswer The user's answer
+     * @return true if the user's answer is correct, false if not
+     */
     public boolean isCorrect(int userAnswer) {
-        return ((userAnswer-1)==correctAnswer);
+        return ((userAnswer - 1) == correctAnswer);
     }
 
+    /**
+     * Returns the correct answer for the question
+     * 
+     * @return A String representing the correct answer
+     */
     public String getCorrectAnswer() {
         return answers.get(correctAnswer);
     }
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Creates a new TriviaGame
+ * 
+ * @author Finlay Palmer
+ */
 public class TriviaGame {
     private static TriviaGame triviaGame;
     private int score;
@@ -8,6 +13,9 @@ public class TriviaGame {
     private Random rand;
     private ArrayList<Question> questions;
 
+    /**
+     * Creates a new TriviaGame
+     */
     private TriviaGame() {
         score = 0;
         numGames = 0;
@@ -15,6 +23,11 @@ public class TriviaGame {
         questions = DataLoader.getTriviaQuestions();
     }
 
+    /**
+     * Get the instance of TriviaGame and create it if the instance is null
+     * 
+     * @return the instance of TriviaGame
+     */
     public static TriviaGame getInstance() {
         if (triviaGame == null) {
             triviaGame = new TriviaGame();
@@ -22,6 +35,11 @@ public class TriviaGame {
         return triviaGame;
     }
 
+    /**
+     * Returns a random question from questions
+     * 
+     * @return A random question from questions
+     */
     public Question getQuestion() {
         if (hasMoreQuestions()) {
             return questions.remove(rand.nextInt(questions.size()));
@@ -30,19 +48,35 @@ public class TriviaGame {
         return null;
     }
 
+    /**
+     * Returns true if there are more questions in questions
+     * 
+     * @return True if there are more questions in questions, false if there are not
+     */
     public boolean hasMoreQuestions() {
         return (questions.size() > 0);
     }
 
+    /**
+     * Increases score and number of games playes
+     */
     public void winRound() {
         score++;
         numGames++;
     }
 
+    /**
+     * Increases number of games played
+     */
     public void loseRound() {
         numGames++;
     }
 
+    /**
+     * Gets a String of the score
+     * 
+     * @return A String represting the score out of number of games
+     */
     public String getScores() {
         return "You scored " + score + "/" + numGames;
     }
